91d3094ba3ed6ec27e4a62453cd6d4b2ee905fd0
Bat Mox <46634694+squid5@users.noreply.github.com>
2024-10-25 14:12:48 -0400

    Add files via upload

---
+++ Content/Calamity/Projectiles/TarragonBloom.cs
@@ -0,0 +1,34 @@
+﻿using FargowiltasCrossmod.Core;
+using FargowiltasCrossmod.Core.Calamity.Globals;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.GameContent;
+using Terraria.ModLoader;
+
+namespace FargowiltasCrossmod.Content.Calamity.Projectiles
+{
+    public class TarragonBloom : ModNPC
+    {
+        public override void SetDefaults()
+        {
+            NPC.width = 46;
+            NPC.height = 48;
+            NPC.damage = 0;
+            NPC.defense = 6;
+            NPC.lifeMax = 999999999;
+            NPC.HitSound = SoundID.NPCHit1;
+            NPC.DeathSound = SoundID.NPCDeath2;
+            NPC.value = 60f;
+            NPC.knockBackResist = 0f;
+            NPC.aiStyle = 523;
+            NPC.noGravity = true;
+        }
+    }
+}
